c2d607931078d7015f3fa8bf77d7dda5b51063c3
Manoj Kumar Patel <112708285+patelmanojkumar@users.noreply.github.com>
2025-02-17 12:18:59 +0530

    Update scripts.js
    added logo

--- scripts.js
+++ scripts.js
@@ -3,3 +3,10 @@ document.querySelectorAll('#notice-board ul li').forEach(item => {
         alert('You clicked on a notice!');
     });
 });
+document.addEventListener("DOMContentLoaded", function() {
+    var link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = 'main/images/favicon.ico';
+    link.type = 'image/x-icon';
+    document.head.appendChild(link);
+});
